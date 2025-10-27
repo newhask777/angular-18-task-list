6c178ee1f94904befad21ea766da907322ac65cf
newhask777 <newhaskellisp@gmail.com>
2025-10-27 08:55:58 +0300

    last

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css' 
 })
-export class UserComponent {
 
+export class UserComponent {
+  @Input({required: true})  avatar!: string
+  @Input({required: true})  name!: string
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
+
+  onSelectedUser() {
+
+  }
 }
