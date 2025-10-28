23f8725bd791e5367f71bae04122d5bd55050d9a
newhask777 <newhaskellisp@gmail.com>
2025-10-28 11:51:58 +0300

    last

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,24 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
+// type User = {
+//   id: string,
+//   name: string,
+//   avatar: string
+// }
 
+interface User {
+  id: string,
+  name: string,
+  avatar: string
+}
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css' 
+  styleUrl: './user.component.css'
 })
 
 export class UserComponent {
-  @Input({required: true})  avatar!: string
-  @Input({required: true})  name!: string
+  @Input({required: true}) user!: User
+  @Output() select = new EventEmitter<string>()
 
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
-  onSelectedUser() {
+  // Sinal approuch
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+  //select = output<string>()
 
+ 
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar()
+  // })
+
+  onSelecteUser() {
+    this.select.emit(this.user.id)
   }
 }
